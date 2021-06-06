abfb4c61997f9a58c015b7c728f9eab1c69d4a13
Sarah Ali <asarahali00@gmail.com>
2021-06-05 22:30:20 -0500

    simplify more

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -2,10 +2,10 @@ console.log("javascript running")
 
 function sendMessage() {
     window.top.postMessage(
-        JSON.stringify({
+        {
             x: 0,
             y: 0
-        }),
+        },
         '*'
     );
 }
