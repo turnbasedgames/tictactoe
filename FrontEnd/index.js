c1f4e1f65c90dd5f948d131ed5efb21c4cf0b29e
Sarah Ali <asarahali00@gmail.com>
2021-06-05 21:37:26 -0500

    adding a message

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,1 +1,16 @@
 console.log("javascript running")
+
+document.getElementById('playerMove').onclick = () => {
+    window.top.postMessage(
+        JSON.stringify({
+            error: false,
+            message: {
+                testFunc: function () {
+                    console.log("clicked")
+                },
+                etc: "message"
+            }
+        }),
+        '*'
+    );
+}
